a8da41a0867e5e20b6d0d809d423cf82bb62cd31
Diego Rocha <drochaesc@gmail.com>
2026-09-10 23:46:49 -0600

    feat(calc): add power, square root and percentage

--- backend/internal/calc/calc.go
+++ backend/internal/calc/calc.go
@@ -1,8 +1,6 @@
 package calc
 
-import "errors"
-
-var ErrDivisionByZero = errors.New("division by zero")
+import "math"
 
 func Add(a, b float64) float64 {
 	return a + b
@@ -21,4 +19,19 @@ func Divide(a, b float64) (float64, error) {
 		return 0, ErrDivisionByZero
 	}
 	return a / b, nil
+}
+
+func Power(a, b float64) (float64, error) {
+	return math.Pow(a, b), nil
+}
+
+func SquareRoot(a float64) (float64, error) {
+	if a < 0 {
+		return 0, ErrNegativeSquareRoot
+	}
+	return math.Sqrt(a), nil
+}
+
+func Percentage(a, b float64) float64 {
+	return a * b / 100
 }
